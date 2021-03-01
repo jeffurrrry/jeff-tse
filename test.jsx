f7e87e6490ba8c1fe0b3ac55cf59c79f89168c09
tse-jeff <jtse0316@gmail.com>
2021-03-01 01:33:48 -0500

    testing react

---
+++ test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const intro = (
+<div>
+    <h1>Hello World</h1>
+    <p>Just a quick test for the basics of JSX and
+        the react library.
+    </p>
+</div>
+);
+
+ReactDOM.render(intro, document.getElementById('root'));
