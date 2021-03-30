cccb78bce2541abc7d045042bea9f8e236a3a688
tse-jeff <jtse0316@gmail.com>
2021-03-30 02:18:50 -0400

    added sine movement to jsbird

--- resources/js/birdAnimation.js
+++ resources/js/birdAnimation.js
@@ -1,20 +1,28 @@
 var id = null;
-      var elem = document.getElementById("jsBird");
-      elem.style.backgroundImage(URL("../images/bird_side.png"));
-      elem.style.width = 50 + "px";
-      elem.style.top = 300 + "px";
-      var pos = 0;
-      var adder = .1;
-      // var off = False;
-      clearInterval(id);
-      id = setInterval(frame, 5);
-      function frame() {
-        if (pos >= 100) {
-          pos = 99;
-          adder *= -1;
-        }
-        else {
-          pos += adder;
-          elem.style.left = pos + "vw";
-        }
-      }
+var elem = document.getElementById("jsBird");
+// elem.style.width = "500px";
+// elem.style.backgroundImage = "url(../images/bird_side.png)";
+var posX = 0;
+var posY = elem.style.getPropertyValue("top");
+posY = posY.substring(0, posY.length - 2);
+var adder = .05;
+// var off = False;
+clearInterval(id);
+id = setInterval(frame, 1);
+function frame() {
+  if (posX >= 100) {
+    posX = 99;
+    adder *= -1;
+    elem.style.transform = "scaleX(-1)";
+  }
+  else if (posX <= -5) {
+    posX = -4;
+    adder *= -1
+    elem.style.transform = "scaleX(1)";
+  }
+  else {
+    posX += adder;
+    elem.style.left = posX + "vw";
+    elem.style.top = String(parseInt(posY) + 30 * Math.sin(posX * 2 * Math.PI / 75)) + "px";
+  }
+}
