bc9054c7355f9434c4c446cd2ad647e37d7b561b
tse-jeff <jtse0316@gmail.com>
2021-03-31 01:15:46 -0400

    progess on multiple js birds

--- resources/js/birdAnimation.js
+++ resources/js/birdAnimation.js
@@ -1,12 +1,30 @@
 var id = null;
+const numOfBirds = 3;
+
 var elem = document.getElementById("jsBird");
-// elem.style.width = "500px";
-// elem.style.backgroundImage = "url(../images/bird_side.png)";
+
+// adds the inputed number of identical birds to an array
+function createBirds(num) {
+  var bird;
+  for (var i = 0; i < num; i++) {
+    bird[i] = document.createElement("img");
+    bird[i].src = "../images/bird_side.png";
+  }
+  return bird;
+}
+
+var bird = createBirds(numOfBirds);
+
+// appends all the birds in the bird array to the elem
+for (var i = 0; i < numOfBirds; i++) {
+  elem.appendChild(bird[i]);
+}
+
+// animating the bird (have to figure out how to do it to each bird)
 var posX = 0;
 var posY = elem.style.getPropertyValue("top");
 posY = posY.substring(0, posY.length - 2);
 var adder = .05;
-// var off = False;
 clearInterval(id);
 id = setInterval(frame, 1);
 function frame() {
@@ -23,6 +41,7 @@ function frame() {
   else {
     posX += adder;
     elem.style.left = posX + "vw";
-    elem.style.top = String(parseInt(posY) + 30 * Math.sin(posX * 2 * Math.PI / 75)) + "px";
+    elem.style.top = String(parseInt(posY) + 30 
+                      * Math.sin(posX * 2 * Math.PI / 75)) + "px";
   }
 }
